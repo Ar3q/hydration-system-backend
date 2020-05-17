c9cce855ea6c5dbfa9d74e844f3a27edc5886a25
Ar3q <arek0906@protonmail.com>
2020-05-17 10:38:10 +0200

    Now running workflow only after push to master. Also removed console.log

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log('envs', process.env.MQTT_HOST, process.env.MQTT_PORT);
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.MQTT,
     options: {
